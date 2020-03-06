97f8a367de0c24824efc444f43b9b52f128e005a
Aushev <dmitryaushev@gmail.com>
2020-03-06 22:20:45 +0200

    Add matchInt(), matchString() methods in Command

--- src/main/java/com/jdbc/config/Command.java
+++ src/main/java/com/jdbc/config/Command.java
@@ -1,5 +1,8 @@
 package com.jdbc.config;
 
+import java.util.List;
+import java.util.Set;
+
 public interface Command {
 
     String command();
@@ -7,5 +10,27 @@ public interface Command {
 
     default boolean canProcess(String input) {
         return input.trim().equals(command());
+    }
+
+    default boolean matchInt(int i, List<Integer> list) {
+
+        boolean match = false;
+        for (int id : list)
+            if (id == i) {
+                match = true;
+                break;
+            }
+        return match;
+    }
+
+    default boolean matchString(String s, Set<String> set) {
+
+        boolean match = false;
+        for (String string : set)
+            if(string.equals(s)) {
+                match = true;
+                break;
+            }
+        return match;
     }
 }
